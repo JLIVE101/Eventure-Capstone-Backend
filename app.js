1376162c5d97ed829b7e68acce2b15c7329055a4
Joshua Williams <jwilliams199055@gmail.com>
2016-09-19 03:08:47 -0400

    added code to app.js for passport.js

--- app.js
+++ app.js
@@ -2,29 +2,57 @@
 // get the packages we need
 // ========================
 
-var express    = require('express');
-var bodyparser = require('body-parser');
-var app        = express();
-var config     = require('./config');
-var routes     = require('./routes/index');
+var config       = require('./config');
+var express      = require('express');
+var app          = express();
+var port         = process.env.PORT || 9000;
+var passport     = require('passport');
+var flash        = require('connect-flash');
+var mw           = require('./helpers/middleware');
+
+var apiRoutes    = require('./routes/api');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyparser   = require('body-parser');
+var session      = require('express-session');
+
+
 
 // =======================
 // configuration =========
 // =======================
-app.set('superSecret', config.secret); // secret variable
+app.set('jwt-secret', config.jwt.secret); // secret variable
+app.set('port', port);
 
 // use body parser so we can get info from POST and/or URL parameters
+app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
+
+//required for passport
+app.use(session({ secret: config.passport.secret })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+require('./passport/index')(passport);
+
 
 // ========================
 // setup routes for app ===
 // ========================
 
-app.get('/', function(req, res) {
-    res.send('Hello! The API is at http://localhost:' + port + '/api');
-});
-app.use('/api/v1', routes);
+//allow only api calls within the array of accepted domains
+app.use(mw.rejectUnwantedDomains);
+
+//wire up passsport.js routes
+require('./routes/index')(app, passport);
+
+
+
+
+//all api routes
+app.use('/api/v1', apiRoutes);
 
 
 module.exports = app;
