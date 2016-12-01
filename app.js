1ac3cec189edbd5baaf6bbbc0afdea4ccd400bd1
Joshua Williams <jwilliams199055@gmail.com>
2016-12-01 06:46:59 -0500

    added notification sockets, models, and routes, updated cors using new package, changed fbook callback to profile page

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ var app = express();
 var port = process.env.PORT || 9000;
 var passport = require('passport');
 var mw = require('./helpers/middleware');
+var cors = require('cors');
 
 var apiRoutes = require('./routes/api');
+
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyparser = require('body-parser');
@@ -26,22 +28,27 @@ var session = require('express-session');
 app.set('jwt-secret', config.jwt.secret); // secret variable
 app.set('port', port);
 
-// allow cross origin requests
-app.all("*", function (req, res, next) {
-	var allowedOrigins = ['http://localhost:3000', 'http://jlive.me', 'http://67.205.153.9'];
-	var origin = req.headers.origin;
-	console.log("host is: " + req.host);
 
-	if(allowedOrigins.indexOf(origin) > -1) {
-		res.setHeader('Access-Control-Allow-Origin', origin);
-	}
-	res.header("Access-Control-Allow-Credentials", "true");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.header("Access-Control-Allow-Methods", "GET, PUT, POST", "DELETE", "OPTIONS");
-	return next();
-});
-//set up static server to server any uploaded images
+//setup cors
+var whitelist =  ['http://localhost:3000', 'http://jlive.me', 'http://67.205.153.9', 'http://67.205.153.9:3000', 'http://localhost'];
+var corsOptions = {
+  origin: function(origin, callback){
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+  },
+	credentials: true,
+};
+
+
+//setup static upload folder
 app.use('/uploads', express.static('uploads'));
+
+
+//setup cors across server
+app.all('*', cors(corsOptions));
+
+
+//set up static server to server any uploaded images
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(morgan('dev'));
